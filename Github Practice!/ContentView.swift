3b232d3afceb0bccb50b295b1d67c9b6d5db2ac5
Sonya <sonyahasthantra@gmail.com>
2023-06-12 14:23:14 -0700

    2nd Commit

--- Github Practice!/ContentView.swift
+++ Github Practice!/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, hi!")
+            Text("Hello, hello!")
         }
         .padding()
     }
