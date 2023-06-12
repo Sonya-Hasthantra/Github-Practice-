7571344813835c067de9ea6da99bd42744af36a1
Sonya <sonyahasthantra@gmail.com>
2023-06-12 14:02:35 -0700

    Initial Commit

---
+++ Github Practice!/Github_Practice_App.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Practice_App.swift
+//  Github Practice!
+//
+//  Created by Dilan Hasthantra on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_Practice_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
