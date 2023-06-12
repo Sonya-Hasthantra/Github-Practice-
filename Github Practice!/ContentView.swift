adbd0479908a362b9dc80402d050c1ffe97661cf
tanya <tanya.dhast@gmail.com>
2023-06-12 14:51:14 -0700

    edit1

--- Github Practice!/ContentView.swift
+++ Github Practice!/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, hello!")
+            Text("Hello, this is Tanya")
         }
         .padding()
     }
